365d370ce1e0456dc3544d792dc5a7a2da302a6e
Matt George <mgeorge@gmail.com>
2016-02-28 20:54:12 -0700

    removing duplicated code

--- gateway_manager/devserver.py
+++ gateway_manager/devserver.py
@@ -56,48 +56,6 @@ def run_function(request, name):
     return module.handle(event, {})
 
 
-def _parse_response(response):
-    response.pattern = response.raw[response.code].get('(selectionPattern)', None)  # NOQA
-    return response
-
-
-def _parse_resource(resource):
-    if getattr(resource, 'method'):
-        resource.handler = resource.raw[resource.method].get('(handler)')
-        resource.responses = map(_parse_response, resource.responses)
-    return resource
-
-
-def parse_annotations(resources):
-    return map(_parse_resource, resources)
-
-
-def build_parent_path(resource):
-    if resource.parent is None:
-        return ''
-    else:
-        return resource.parent.path
-
-
-def remove_prefix(prefix, path):
-    if prefix and path.startswith(prefix):
-        return path[len(prefix):]
-    return path
-
-
-def path_part(resource):
-    parent_path = build_parent_path(resource)
-    path_part = remove_prefix(parent_path, resource.path)
-    path_part = remove_prefix('/', path_part)
-    resource.path_part = path_part
-    return resource
-
-
-def transform_resources(resources):
-    resources = parse_annotations(resources)
-    return map(path_part, resources)
-
-
 def _filter_absent_methods(resource):
     return getattr(resource, 'method') is not None
 
@@ -160,7 +118,6 @@ def build_lookup_table(resources):
 def build_wsgi_app(raml_file):
     # read raml, create router
     raml = ramlfications.parse(raml_file)
-    # resources = transform_resources(raml.resources)
     resources = api.transform_resources(raml.resources)
     resources = filter(_filter_absent_methods, resources)
     build_lookup_table(resources)
